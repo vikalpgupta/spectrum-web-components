8a3cd94c99fc789561686169eb9d57f5d592723b
Megan Logsdon <megan@heysparkbox.com>
2023-03-10 16:39:45 -0500

    fix(textfield): add grows attribute to config

--- packages/textfield/src/spectrum-config.js
+++ packages/textfield/src/spectrum-config.js
@@ -33,15 +33,63 @@ export default {
                 converter.classToId('spectrum-Textfield', 'textfield'),
                 converter.classToClass('spectrum-Textfield-input', 'input'),
                 {
-                    find: [
-                        builder.class('spectrum-Textfield-input'),
-                        builder.class('focus-ring'),
-                    ],
-                    replace: [
-                        { replace: builder.class('input') },
-                        { replace: builder.pseudoClass('focus-visible') },
-                    ],
-                    hoist: false,
+                    selector: '.spectrum-Textfield-input',
+                    name: 'input',
+                },
+                {
+                    selector: '.spectrum-Textfield-validationIcon',
+                    name: 'icon',
+                },
+                {
+                    selector: '.spectrum-Textfield-icon',
+                    name: 'icon-workflow',
+                },
+            ],
+            attributes: [
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-Textfield--multiline',
+                    name: 'multiline',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-Textfield--grow',
+                    name: 'grows',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-Textfield--quiet',
+                    name: 'quiet',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-focused',
+                    name: 'focused',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-keyboardFocused',
+                    name: 'focused',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-valid',
+                    name: 'valid',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-invalid',
+                    name: 'invalid',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-disabled',
+                    name: 'disabled',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-readOnly',
+                    name: 'readonly',
                 },
                 converter.classToClass(
                     'spectrum-Textfield-validationIcon',
